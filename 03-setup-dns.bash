b626fe85483808dda09613bab925dd3dbd5d4eef
seubpong.mon <pjame.fb@gmail.com>
2022-03-19 21:26:08 +0700

    Added A record

--- 03-setup-dns.bash
+++ 03-setup-dns.bash
@@ -2,10 +2,31 @@
 
 source .env
 
+IP_NAME=ingress-ip-1
+
 # Create IP
-gcloud compute addresses create ingress-ip-1 \
+gcloud compute addresses create ${IP_NAME} \
 --global \
 --ip-version IPV4 \
 --project ${PROJECT}
 
+IP=$(gcloud compute addresses describe ${IP_NAME} --global --project=${PROJECT} | grep 'address:' | cut -d':' -f2)
+
 # Add A record to DNS
+gcloud dns record-sets transaction start --zone=${MANAGED_ZONE} --project ${PROJECT}
+
+gcloud dns record-sets transaction add ${IP} \
+   --name=${DOMAIN} \
+   --ttl=300 \
+   --type=A \
+   --zone=${MANAGED_ZONE} \
+   --project ${PROJECT}
+
+gcloud dns record-sets transaction add ${IP} \
+   --name="*.${DOMAIN}" \
+   --ttl=300 \
+   --type=A \
+   --zone=${MANAGED_ZONE} \
+   --project ${PROJECT}
+
+gcloud dns record-sets transaction execute --zone=${MANAGED_ZONE} --project ${PROJECT}
