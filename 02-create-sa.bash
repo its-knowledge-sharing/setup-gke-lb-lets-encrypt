577b43fb7e04838cfdd9f0ddb38ac341aecef7c6
seubpong.mon <pjame.fb@gmail.com>
2022-03-19 17:16:56 +0700

    Added cert-manager

--- 02-create-sa.bash
+++ 02-create-sa.bash
@@ -2,5 +2,9 @@
 
 source .env
 
-gsutil mb -b on -l us-central1 -p ${PROJECT} gs://${BUCKET_NAME}/ 
-gsutil ls gs://${BUCKET_NAME}/
+gcloud iam service-accounts create ${SA_NAME} --project ${PROJECT} --display-name="Service account for GKE"
+
+gcloud projects add-iam-policy-binding ${PROJECT} \
+    --member="serviceAccount:${SA_NAME}@${PROJECT}.iam.gserviceaccount.com" \
+    --project ${PROJECT} \
+    --role="roles/dns.admin"
--- 03-deploy-cert-manager.bash
+++ 03-deploy-cert-manager.bash
@@ -2,14 +2,27 @@
 
 source .env
 
+KEY_FILE=service-account.json
+SA=${SA_NAME}@${PROJECT}.iam.gserviceaccount.com
+SECRET=gcp-cloud-dns-account-key
+
 NS=cert-manager
 kubectl create ns ${NS}
+
+# Create service account secret
+gcloud iam service-accounts keys create ${KEY_FILE} --iam-account=${SA}
+kubectl delete secret ${SECRET} -n ${NS}
+kubectl create secret generic ${SECRET} --from-file=${KEY_FILE}=${KEY_FILE} -n ${NS}
 
 helm repo add cert-manager https://charts.jetstack.io/
 
 helm template cert-manager cert-manager/cert-manager \
--f cert-manager/cert-manager.yaml \
---version 1.7.1 \
---namespace ${NS} > tmp-cert-manager.yaml
+    -f cert-manager/cert-manager.yaml \
+    --version 1.7.1 \
+    --namespace ${NS} > tmp-cert-manager.yaml
 
 kubectl apply -n ${NS} -f tmp-cert-manager.yaml
+
+CLUSTER_ISSUER_FILE=cert-manager/cluster-issuer.yaml
+sed -i "s#__PROJECT__#${PROJECT}#g" ${CLUSTER_ISSUER_FILE}
+kubectl apply -n ${NS} -f ${CLUSTER_ISSUER_FILE}
