8d07310dbfed93a1cae868da8e6b140489c7b2aa
seubpong.mon <pjame.fb@gmail.com>
2022-03-19 20:09:32 +0700

    Added nginx ingress

---
+++ 05-deploy-nginx-ingress.bash
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+NS=ingress-nginx
+
+echo "####"
+echo "#### Deploying nginx config to [${NS}] ####"
+
+kubectl create ns ${NS}
+
+kubectl apply -f nginx-backend-cfg.yaml -n ${NS}
+
+OUTPUT_FILE=nginx-ing-1.yaml
+sed -i "s#<<DOMAIN>>#${DOMAIN}#g" ${OUTPUT_FILE}
+kubectl apply -f ${OUTPUT_FILE} -n ${NS}
+
+
+helm repo add nginx https://kubernetes.github.io/ingress-nginx/
+
+helm template nginx nginx/ingress-nginx \
+    -f nginx-ingress/nginx.yaml \
+    --version 4.0.2 \
+    --namespace ${NS} > tmp-nginx.yaml
+
+kubectl apply -n ${NS} -f tmp-nginx.yaml
