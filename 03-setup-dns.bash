9d923b65f3165ed47560c8d8ea8696e8c7cee458
seubpong.mon <pjame.fb@gmail.com>
2022-03-19 21:04:47 +0700

    Added IP address

--- 03-setup-dns.bash
+++ 03-setup-dns.bash
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+source .env
+
 # Create IP
 gcloud compute addresses create ingress-ip-1 \
 --global \
---ip-version IPV4
+--ip-version IPV4 \
+--project ${PROJECT}
 
 # Add A record to DNS
