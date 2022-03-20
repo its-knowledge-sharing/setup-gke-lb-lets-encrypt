084772c680f1fb75ddc061e5c8c67e3d967c0340
seubpong.mon <pjame.fb@gmail.com>
2022-03-20 13:33:36 +0700

    Added new domain test3

--- 08-2-update-gcp-managed-cert.bash
+++ 08-2-update-gcp-managed-cert.bash
@@ -15,6 +15,7 @@ kubectl apply -f ${OUTPUT_FILE}.tmp -n ${NS}
 
 # Create indenpendent ManagedCertificate without linking to LB will not work
 # If we link ManagedCertificate to existing LB, it will cause down time.
+# It need to link to LB because it needs IP address for Verifying the domain
 
 OUTPUT_FILE=nginx-ingress/nginx-ing-2.yaml
 cp ${OUTPUT_FILE} ${OUTPUT_FILE}.tmp
