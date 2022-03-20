2686ef97dab567a828c5bd08f5a3116ff1fd3049
seubpong.mon <pjame.fb@gmail.com>
2022-03-20 10:44:03 +0700

    Added new sud domain to zones

--- 07-1-update-dns.bash
+++ 07-1-update-dns.bash
@@ -10,13 +10,6 @@ IP=$(gcloud compute addresses describe ${IP_NAME} --global --project=${PROJECT} 
 gcloud dns record-sets transaction start --zone=${MANAGED_ZONE1} --project ${PROJECT}
 
 gcloud dns record-sets transaction add ${IP} \
-   --name=${DOMAIN1} \
-   --ttl=300 \
-   --type=A \
-   --zone=${MANAGED_ZONE1} \
-   --project ${PROJECT}
-
-gcloud dns record-sets transaction add ${IP} \
    --name="test2.${DOMAIN1}" \
    --ttl=300 \
    --type=A \
--- 07-2-update-dns.bash
+++ 07-2-update-dns.bash
@@ -10,13 +10,6 @@ IP=$(gcloud compute addresses describe ${IP_NAME} --global --project=${PROJECT} 
 gcloud dns record-sets transaction start --zone=${MANAGED_ZONE2} --project ${PROJECT}
 
 gcloud dns record-sets transaction add ${IP} \
-   --name=${DOMAIN2} \
-   --ttl=300 \
-   --type=A \
-   --zone=${MANAGED_ZONE2} \
-   --project ${PROJECT}
-
-gcloud dns record-sets transaction add ${IP} \
    --name="test2.${DOMAIN2}" \
    --ttl=300 \
    --type=A \
