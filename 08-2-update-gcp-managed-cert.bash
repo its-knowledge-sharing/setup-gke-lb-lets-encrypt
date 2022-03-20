b5cee236263625907f7edf617acb8f3f479b6291
seubpong.mon <pjame.fb@gmail.com>
2022-03-20 11:41:30 +0700

    Added new domain test3

--- 08-2-update-gcp-managed-cert.bash
+++ 08-2-update-gcp-managed-cert.bash
@@ -8,7 +8,7 @@ NS=ingress-nginx
 # Same ingress but new ManagedCertificate
 # Caused a little down time
 
-OUTPUT_FILE=nginx-ingress/gcp-manage-certs-2.yaml
+OUTPUT_FILE=nginx-ingress/gcp-manage-certs-3.yaml
 cp ${OUTPUT_FILE} ${OUTPUT_FILE}.tmp
 sed -i "s#<<DOMAIN>>#${DOMAIN2}#g" ${OUTPUT_FILE}.tmp
 kubectl apply -f ${OUTPUT_FILE}.tmp -n ${NS}
