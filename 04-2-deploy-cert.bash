ce7cec0f236b188adcb581ce6cb7b7dd85fab6b8
seubpong.mon <pjame.fb@gmail.com>
2022-03-20 10:01:02 +0700

    Refactored code

--- 04-2-deploy-cert.bash
+++ 04-2-deploy-cert.bash
@@ -6,5 +6,5 @@ NS=ingress-nginx
 
 OUTPUT_FILE=certs/demo-certificate.yaml
 cp ${OUTPUT_FILE} ${OUTPUT_FILE}.tmp
-sed -i "s#<<DOMAIN>>#${DOMAIN}#g" ${OUTPUT_FILE}.tmp
+sed -i "s#<<DOMAIN>>#${DOMAIN1}#g" ${OUTPUT_FILE}.tmp
 kubectl apply -n ${NS} -f ${OUTPUT_FILE}.tmp
