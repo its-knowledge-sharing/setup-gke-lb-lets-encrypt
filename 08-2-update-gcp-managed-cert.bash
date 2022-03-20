217d7f14446b25ce89172a8097631f6b4d44dc08
seubpong.mon <pjame.fb@gmail.com>
2022-03-20 11:26:44 +0700

    Use old ManagedCertificate

--- 08-2-update-gcp-managed-cert.bash
+++ 08-2-update-gcp-managed-cert.bash
@@ -6,13 +6,14 @@ NS=ingress-nginx
 
 # Create new ManagedCertificate instead of update the existing one
 # Same ingress but new ManagedCertificate
+# Caused a little down time
 
 OUTPUT_FILE=nginx-ingress/gcp-manage-certs-2.yaml
 cp ${OUTPUT_FILE} ${OUTPUT_FILE}.tmp
 sed -i "s#<<DOMAIN>>#${DOMAIN2}#g" ${OUTPUT_FILE}.tmp
 kubectl apply -f ${OUTPUT_FILE}.tmp -n ${NS}
 
-OUTPUT_FILE=nginx-ingress/nginx-ing-2-1.yaml
+OUTPUT_FILE=nginx-ingress/nginx-ing-2.yaml
 cp ${OUTPUT_FILE} ${OUTPUT_FILE}.tmp
 sed -i "s#<<DOMAIN>>#${DOMAIN2}#g" ${OUTPUT_FILE}.tmp
 kubectl apply -f ${OUTPUT_FILE}.tmp -n ${NS}
